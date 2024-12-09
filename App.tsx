9fd4f3efdb9a3088dab40b978f725cfefa1cbae3
“ParkChangHee” <arrans9591@gmail.com>
2024-12-10 01:55:47 +0900

    splash screen 제작하기

--- App.tsx
+++ App.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
 import StackNavigator from './navigators/StackNavigator';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
 const App: React.FC = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // 폰트 로딩
+        await Font.loadAsync(Entypo.font);
+        // 추가 초기화 작업이 필요하다면 여기에 추가
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // 스플래시 스크린 숨기기
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
   return (
     <NavigationContainer>
       <StackNavigator />
